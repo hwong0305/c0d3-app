ef8940cf0cfbda42a2bdc9c690973260ab6f7a5f
hwong0305 <hwong.0305@gmail.com>
2020-05-11 07:34:51 +0000

    updated icon using octicons

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ReactComponent as Checked } from '../assets/images/checked.svg'
-import { ReactComponent as Failed } from '../assets/images/error.svg'
+import OctiIcon, { IssueOpened } from '@primer/octicons-react'
 
 type Props = {
   children?: React.ReactNode
@@ -22,7 +22,12 @@ const Card: React.FC<Props> = props => {
             <Checked className="mb-4" width="100px" height="100px" />
           )}
           {props.fail && (
-            <Failed className="mb-4 text-danger" height="100px" width="100px" />
+            <OctiIcon
+              icon={IssueOpened}
+              className="mb-4 text-danger"
+              height={100}
+              width={100}
+            />
           )}
           <h1 className="card-title h2 font-weight-bold mb-5">{props.title}</h1>
           <p className="card-text">{props.text}</p>
