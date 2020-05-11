b93a7ebf46517ade656a524ae480f0bf80b3b6bf
hwong0305 <hwong.0305@gmail.com>
2020-05-11 05:29:45 +0000

    Added story for success rename login story

---
+++ stories/Success.stories.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import SuccessComponent from '../pages/success'
+
+export default {
+  title: 'Pages/success'
+}
+
+export const Success: React.FC = () => <SuccessComponent />
